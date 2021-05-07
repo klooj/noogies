599f0cf523aa8c58a7602b05cfe116c66988eb8b
klooj <57016271+klooj@users.noreply.github.com>
2021-05-07 01:12:55 -0400

    take2

--- lua/noog1.lua
+++ lua/noog1.lua
@@ -1,48 +1,44 @@
 -- local vg = vim.g
--- local colors = require('colorbuddy.color').colors
-local col = {
-  base0 = '#1B2229',
-  base1 = '#1c1f24',
-  base2 = '#202328',
-  base3 = '#23272e',
-  base4 = '#3f444a',
-  base5 = '#5B6268',
-  base6 = '#73797e',
-  base7 = '#9ca0a4',
-  base8 = '#b1b1b1',
+local colors = require('colorbuddy.color').colors
+-- local col = {
+ colors('base0','#1B2229')
+ colors('base1','#1c1f24')
+ colors('base2','#202328')
+ colors('base3','#23272e')
+ colors('base4','#3f444a')
+ colors('base5','#5B6268')
+ colors('base6','#73797e')
+ colors('base7','#9ca0a4')
+ colors('base8','#b1b1b1')
+ colors('bg','#282c34')
+ colors('bg1','#504945')
+ colors('bg_highlight','#2E323C')
+ colors('bg_visual','#b3deef')
+ colors('bg_tree','#050120')
+ colors('bg_cursor','#4f5b66')
+ colors('bg_popup','#3E4556')
+ colors('wk_popup','#010010')
+ colors('fg','#bbc2cf')
+ colors('fg_alt','#5B6268')
+ colors('black','#000000')
+ colors('blue','#51afef')
+ colors('brick','#824020')
+ colors('brown','#fab795')
+ colors('cyan','#44bcc9')
+ colors('dark_blue','#6e87c4')
+ colors('gold','#f0c634')
+ colors('green','#98be65')
+ colors('grey','#928374')
+ colors('indigo','#6666dc')
+ colors('magenta','#d16d9e')
+ colors('orange','#D98E48')
+ colors('purple','#b877db')
+ colors('red','#e95678')
+ colors('teal','#1abc9c')
+ colors('violet','#a9a1e1')
+ colors('yellow','#f0c674')
+ colors('bracket','#80A0C2')
+ colors('none','NONE')
+-- }
 
-  bg = '#282c34',
-  bg1 = '#504945',
-  bg_highlight = '#2E323C',
-  bg_visual = '#b3deef',
-  bg_tree = '#050120',
-  bg_cursor = '#4f5b66',
-  bg_popup = '#3E4556',
-  wk_popup = '#010010',
-
-  fg = '#bbc2cf',
-  fg_alt = '#5B6268',
-
-  black = '#000000',
-  blue = '#51afef',
-  brick = '#824020',
-  brown = '#fab795',
-  cyan = '#44bcc9',
-  dark_blue = '#6e87c4',
-  gold = '#f0c634',
-  green = '#98be65',
-  grey = '#928374',
-  indigo = '#6666dc',
-  magenta = '#d16d9e',
-  orange = '#D98E48',
-  purple = '#b877db',
-  red = '#e95678',
-  teal = '#1abc9c',
-  violet = '#a9a1e1',
-  yellow = '#f0c674',
-
-  bracket = '#80A0C2',
-  none = 'NONE'
-}
-
-for k, v in pairs(col) do require('colorbuddy.color').Color.new(k, v) end
+-- for k, v in pairs(col) do require('colorbuddy.color').Color.new(k, v) end
